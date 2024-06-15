15b1cb8350c79872bb65f470e06589bc5cf41af7
Sourabh-R-Chavan <119604646+Sourabh-R-Chavan@users.noreply.github.com>
2024-06-15 12:44:18 +0530

    Initial Commit

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public static void main(String[] args) {
     System.out.println("Output from java program ");
     System.out.println(new Date());
     System.out.println("Helloooooooo!");
+    System.out.println("Helloooooooo!");
 
 }
 }
